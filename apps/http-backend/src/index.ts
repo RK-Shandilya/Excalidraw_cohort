0ad319e10e3b7ce1557e835b29336675c21c1806
Rupesh <rkshandilya001@gmail.com>
2025-01-15 23:20:30 +0530

    completed_backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,43 +3,100 @@ import jwt from 'jsonwebtoken'
 import {JWT_SECRET} from '@repo/backend-common/config'
 import { authMiddleware } from './middleware';
 import client from "@repo/db/client"
+import bcrypt from 'bcryptjs'
 import {CreateUserSchema, SigninSchema, CreateRoomSchema} from '@repo/common/types'
 const PORT = 3000;
 const app = express();
+app.use(express.json());
 
-
-app.post("/signup", (req: Request, res:Response)=>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async(req: Request, res:Response)=>{
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.status(400).json({message: "Invalid request"});
         return;
     }
     // db call
-    res.json({
-        userId: "123",
-        message: "User created"
-    })
+    try {
+        const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+        const user = await client.user.create({
+            data: {
+                name:parsedData.data.name,
+                username: parsedData.data.username,
+                password: hashedPassword
+            }
+        })
+
+        res.json({
+            userId: user.id,
+            message: "User created"
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Failed to create user"});
+    }
 })
 
-app.post("/signin", (req: Request, res:Response)=>{
-    const data = SigninSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signin", async(req: Request, res:Response)=>{
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.status(400).json({message: "Invalid request"});
         return;
     }
-    const userId = 1
-    const token = jwt.sign({userId}, JWT_SECRET, {expiresIn: "1h"});
-    res.json({token});
+    try {
+        const user = await client.user.findUnique({
+            where: {email: parsedData.data.username}
+        })
+        if(!user) {
+            res.status(401).json({message: "Invalid username or password"});
+            return;
+        }
+        const isValidPassword = await bcrypt.compare(parsedData.data.password, user.password);
+        if(!isValidPassword) {
+            res.status(401).json({message: "Invalid password"});
+        }
+        const token = jwt.sign({userId:user.id}, JWT_SECRET, {expiresIn: "1h"});
+        res.json({token});
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({message: "Failed to signin"});
+    }
 })
 
-app.post("/room", authMiddleware, (req: Request, res:Response)=>{
-    const data = CreateRoomSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/room", authMiddleware, async(req: Request, res:Response)=>{
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.status(400).json({message: "Invalid request"});
     }
     // db call
-    res.json({
-        roomId: 12345,
+    try {
+        // @ts-ignore
+        const userId = req.userId
+        const room = await client.room.create({
+            data: {
+                name: parsedData.data?.name,
+                adminId: userId
+            }
+        })
+
+        res.json({
+            roomId: 12345,
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Failed to create room"});
+    }
+})
+
+app.get("/chats/:roomId", async(req: Request, res: Response)=> {
+    const roomId = Number(req.params.roomId);
+    const messages = await client.chat.findMany({
+        where: {
+            roomId: roomId
+        },
+        orderby: {
+            id: "desc"
+        },
+        take: 50
     })
 })
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,23 +1,79 @@
-import { WebSocketServer } from 'ws'
+import WebSocket ,{ WebSocketServer } from 'ws'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import { JWT_SECRET } from '@repo/backend-common/config'
 const wss = new WebSocketServer({ port: 8080 })
+ 
+interface User {
+    ws : WebSocket,
+    rooms: string[],
+    userId: string
+}
 
-wss.on("connection", (ws , request) => {
+const users: User[] = []
+
+function checkUser(token : string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || typeof decoded === 'string' || !decoded.userId) {
+        return null;
+    }
+    return decoded['userId'];
+}
+
+wss.on("connection", (ws: WebSocket , request) => {
     console.log("Client connected");
     const url = request.url;
     if(!url) return;
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get("token") || "";
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const userId = checkUser(token);
 
-    if(!decoded || !decoded.userId) {
+    if(!userId) {
         ws.close();
         return;
     }
 
-    ws.on("message", (message) => {
-        console.log(`Received message => ${message}`)
-        ws.send(`Server received your message: ${message}`)
+    users.push({
+        ws,
+        rooms: [],
+        userId: userId
+    })
+
+    ws.on("message", (data) => {
+        const parsedData = JSON.parse(data.toString());
+        switch (parsedData.type) {
+            case "joinRoom":
+                const currentUser = users.find((x)=>x.ws===ws);
+                if (!currentUser) {
+                    console.error("User not found for WebSocket connection");
+                    return;
+                }
+
+                if (!currentUser.rooms.includes(parsedData.roomId)) {
+                    currentUser.rooms.push(parsedData.roomId);
+                }
+                break;
+            case "leaveRoom":
+                const user = users.find((x)=>x.ws===ws);
+                if (!user) {
+                    console.error("User not found for WebSocket connection");
+                    return;
+                }
+                user.rooms = user.rooms.filter((x)=> x===parsedData.rooms);
+                break;
+
+            case 'chat':
+                const roomId = parsedData.roomId;
+                const message = parsedData.message;
+
+                users.forEach(( user) => {
+                    if (user.rooms.includes(roomId)) {
+                        user.ws.send(JSON.stringify({
+                            type: "chat",
+                            message: message,
+                            roomId
+                        }))
+                    }
+                })
+        }
     })
 })
